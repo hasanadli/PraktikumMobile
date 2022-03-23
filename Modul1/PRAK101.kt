dc16500f1ac5d6df102e107d23354694bf66348e
hasanadli <101862330+hasanadli@users.noreply.github.com>
2022-03-23 20:59:42 +0700

    Add files via upload

---
+++ Modul1/PRAK101.kt
@@ -0,0 +1,26 @@
+import kotlin.random.Random
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val input = Scanner(System.`in`)
+    val waktu: String
+    val nama: String
+    val umur: Int
+    var suhu: String
+    print("Waktu Sekarang: ")
+    waktu = input.nextLine()
+    print("Nama Anda: ")
+    nama = input.nextLine()
+    print("Umur Anda: ")
+    umur = input.nextInt()
+    print("Suhu Anda: ")
+    suhu = input.nextLine()
+    suhu = String.format("%.2f", Random.nextDouble(35.00, 37.00))
+
+
+    println("\n")
+
+    println("Selamat $waktu $nama.")
+    println("Umur anda $umur Tahun.")
+    println("Suhu Tubuh anda $suhu derajat Celcius")
+}
